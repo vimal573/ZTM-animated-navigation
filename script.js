e2ab84ded8c9614814687c2fa7df191e08abbb61
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-06-14 09:54:41 +0530

    Animating nav items

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const nav4 = document.getElementById('nav-4');
 const nav5 = document.getElementById('nav-5');
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+
+// Control Navigation Animation
+function navAnimation(direction1, direction2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(
+      `slide-${direction1}-${i + 1}`,
+      `slide-${direction2}-${i + 1}`
+    );
+  });
+}
 
 function toggleNav() {
   // Toggle: Menu Bars Open/Closed
@@ -26,3 +37,6 @@ function toggleNav() {
 
 // Event Listeners
 menuBars.addEventListener('click', toggleNav);
+navItems.forEach(nav => {
+  nav.addEventListener('click', toggleNav);
+});
